fccb29905d43cbc2312b9519683cb712ac94d432
UhTrue <40493854+UhTrue@users.noreply.github.com>
2021-03-10 17:10:51 -0600

    Update UpvalueScanner.lua

--- ui/modules/UpvalueScanner.lua
+++ ui/modules/UpvalueScanner.lua
@@ -2,45 +2,7 @@ local RunService = game:GetService("RunService")
 local TextService = game:GetService("TextService")
 
 local UpvalueScanner = {}
-local ClosureSpy = import("modules/ClosureSpy")
-local Methods = import("modules/UpvalueScanner")
 
-if not hasMethods(Methods.RequiredMethods) then
-    return UpvalueScanner
-end
-
-local Upvalue = import("objects/Upvalue")
-
-local Prompt = import("ui/controls/Prompt")
-local CheckBox = import("ui/controls/CheckBox")
-local Dropdown = import("ui/controls/Dropdown")
-local List, ListButton = import("ui/controls/List")
-local TabSelector = import("ui/controls/TabSelector")
-local MessageBox, MessageType = import("ui/controls/MessageBox")
-local ContextMenu, ContextMenuButton = import("ui/controls/ContextMenu")
-
-local Base = import("rbxassetid://5042109928").Base
-local Assets = import("rbxassetid://5042114982").UpvalueScanner
-
-local Prompts = Base.Prompts
-local Page = Base.Body.Pages.UpvalueScanner
-
-local SpyHook = ClosureSpy.Hook
-
-local Query = Page.Query
-local Search = Query.Search
-local SearchBox = Query.Query
-local Filters = Page.Filters
-local ResultsClip = Page.Results.Clip
-local ResultStatus = ResultsClip.ResultStatus
-
-local modifyUpvalue = Prompt.new(Prompts.ModifyUpvalue)
-local modifyElement = Prompt.new(Prompts.ModifyElement)
-local deepSearch = CheckBox.new(Filters.SearchInTables)
-local upvalueList = List.new(ResultsClip.Content)
-
-local deepSearchFlag = false
-local currentUpvalues = {}
 
 
 return UpvalueScanner 
